3cc91a47659fcb633274acd2dab102fa32996e77
Rumplest <julian.rojas@rappi.com>
2019-03-09 13:09:46 -0500

    Add connection mongo and env variables

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
 import { Application } from 'express';
+
+import mongoose from 'mongoose';
 import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
 
 import { initRoutes } from './routes';
 
+dotenv.config({ path: path.resolve(`${__dirname}/envs/dev.env`) });
 const PORT = process.env.PORT || 3000;
+const DB_URL = process.env.DB_URL || '';
 
 const app: Application = express();
 
 initRoutes(app);
+
+mongoose.connect(DB_URL, { useNewUrlParser: true })
+    .then(() => console.log('DB online'))
+    .catch((err: Error) => console.log(err));
 
 app.listen(PORT, () => console.log(`Init in ${PORT}`));
